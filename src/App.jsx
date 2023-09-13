4fc2771b38fd6e882d12e2f20330109f9fb2bc73
webdevmahdi <mahdihasan01705259@gmail.com>
2023-09-13 19:55:42 +0600

    sign out setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
+import { useState } from 'react';
 import './App.css';
 import app from './firebase.init';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
 
 
 
 const auth = getAuth(app);
 
 function App() {
+  let [user, setUser] = useState({});
+
   let provider = new GoogleAuthProvider();
-  let signInPopUp = () =>{
+
+  let signInPopUp = () => {
     signInWithPopup(auth, provider)
-    .then(result => {
-      console.log(result);
-    })
-    .catch(error => console.error('error', error));
+      .then(result => {
+        let user = result.user;
+        setUser(user);
+        console.log(result);
+      })
+      .catch(error => console.error('error', error));
+  }
+
+  let signOutAccount = () => {
+    signOut(auth)
+      .then(() => setUser({}))
+      .catch(error => setUser({}));
   }
 
   return (
     <div>
-      <button onClick={signInPopUp}>Google sign in</button>
+      {
+        user.email ?  <button onClick={signOutAccount}>Sign out</button> : <button onClick={signInPopUp}>Google sign in</button>
+          
+      }
+          <div>
+            <img src={user.photoURL} />
+            <h1>Name: {user.displayName}</h1>
+            <h3>Email: {user.email}</h3>
+          </div>
     </div>
   )
 }
